6b3375dbb828e21fe7b097c44bbccf481b49c193
Andres Esparza <andres.esparza@onglared.org>
2023-11-29 12:04:16 -0500

    Mas

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#_xyk&f*^l9t!t@pnf@yqw$%f!$r=t&kx#yc$f(6=wmk)8g(5m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gestionenred.pythonanywhere.com']
 
 
 # Application definition
