96faabd9a7ff250c28e7b463a81a6534960ace3d
Andres Esparza <andres.esparza@onglared.org>
2023-12-15 10:21:59 -0500

    Configuracion base de datos

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -77,10 +77,24 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gestionred$gestired',
+        'USER': 'gestionred',
+        'PASSWORD': 'Lared@821',
+        'HOST': 'gestionred.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRIC_TRANS_TABLES'"
+        },
     }
 }
 
